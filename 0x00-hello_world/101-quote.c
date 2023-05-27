8576c49c55f6da9b5848228955f9a9541c5a4869
El-Tawfik17 <kifwat17@gmail.com>
2023-05-27 13:16:28 -0700

    quotes Done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description:using the main function
+ *this program prints "and that piece of art is useful\"
+ * - Dora Korpar, 2015-10-19"
+ *Return:Always 0 (Success)
+ */
+int main(void)
+{
+	/*string printing*/
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
