39d6c310ef99b9892a93f4965ac72ba5bd5d4b51
El-Tawfik17 <kifwat17@gmail.com>
2023-05-27 11:43:00 -0700

    puts Done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return:Always 0 (Success)
+ */
+int main(void)
+{
+	/*string initialization*/
+	char MyString[] = "Progamming is like building a multilingual puzzle";
+	/*string printing*/
+	puts(MyString);
+	return (0);
+}
