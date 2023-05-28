e13c51c064f32b72e787bbc686b70ae3279a8032
El-Tawfik17 <kifwat17@gmail.com>
2023-05-27 20:34:25 -0700

    task 2 Done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This program prints alphabet
+ * letter in lowcase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*declaring variable*/
+	char c;
+	int i;
+	/*initialisation */
+	c = 'a';
+	/*printing*/
+	while (c < ('a' + 24))
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
