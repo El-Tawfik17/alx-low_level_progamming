37e802cf889a72c16d9d756151bbb0835befc631
El-Tawfik17 <kifwat17@gmail.com>
2023-05-29 01:14:04 -0700

    okay

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,52 +1,25 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: This program prints all
- * single digital numbers of base 10
- * Return: Always 0 (Success)
+ * main - program that prints numbers from 0 to 99.
+ * Return: 0
  */
+
 int main(void)
 {
-	/*declaring variables*/
-	int i;
-	int ii, iii, iiii;
-	/*initialisation */
-	iiii = 1;
-	i = 0;
-	ii = 0;
-	iii = 0;
-	/*printing*/
-	while (i < 10)
-	{
-		while (ii < 10)
-		{
-			while (iii < 10)
-			{
-				while (iiii < 10)
-				{
-					putchar(i + '0');
-					putchar(ii + '0');
-					putchar(' ');
-					putchar(iii + '0');
-					putchar(iiii + '0');
-					if (i <= 9 && ii <= 7 && iii <= 9 && iiii <= 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					iiii++;
-				}
-				iii++;
-				iiii = 0;
-			}
-			ii++;
-			iii = 0;
-		}
-		i++;
-		ii = 0;
-	}
-	putchar('\n');
-	return (0);
-}
+	int c = 0;
 
+	while (c <= 99)
+	{
+		putchar(c / 10 + '0');
+		putchar(c % 10 + '0');
+		if (c != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+putchar('\n');
+return (0);
+}
 
