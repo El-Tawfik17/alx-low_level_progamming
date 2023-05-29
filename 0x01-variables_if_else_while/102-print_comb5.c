efcf2285b05f459f7a8f9e8f823a03348807fdbe
El-Tawfik17 <kifwat17@gmail.com>
2023-05-29 00:20:59 -0700

    task 12 done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This program prints all
+ * single digital numbers of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*declaring variables*/
+	int i;
+	int ii, iii, iiii;
+	/*initialisation */
+	iiii = 1;
+	i = 0;
+	ii = 0;
+	iii = 0;
+	/*printing*/
+	while (i < 10)
+	{
+		while (ii < 10)
+		{
+			while (iii < 10)
+			{
+				while (iiii < 10)
+				{
+					putchar(i + '0');
+					putchar(ii + '0');
+					putchar(' ');
+					putchar(iii + '0');
+					putchar(iiii + '0');
+					if (i <= 9 && ii <= 7 && iii <= 9 && iiii <= 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					iiii++;
+				}
+				iii++;
+			}
+			ii++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
