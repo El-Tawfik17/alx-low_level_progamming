a51189849002ee6080c1b0fc394d462411c4a995
El-Tawfik17 <kifwat17@gmail.com>
2023-05-29 22:42:46 -0700

    task 4 DOne

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - function
+ * Description:This function checks alphabet
+ * it return 1 if it is alphabet and 0 otherwhise
+ * Return:1 or 0
+ */
+int _isalpha(int c)
+{
+	/*declaring variable*/
+	char cc;
+	char CC;
+	/*initialization*/
+	cc = 'a';
+	CC = 'A';
+	while ((cc <= 'z') || (CC <= 'Z'))
+	{
+		if (c == cc || c == CC)
+			return (1);
+		cc++;
+		CC++;
+	}
+	return (0);
+}
