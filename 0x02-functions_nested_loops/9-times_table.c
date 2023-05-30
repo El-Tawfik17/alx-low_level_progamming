c80a7101ce0166d05f6aa96dc7e87de847e9b815
El-Tawfik17 <kifwat17@gmail.com>
2023-05-30 05:18:22 -0700

    task 9 Done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - function
+ * Description:This function prints the 9 times
+ * tables,starting with 0
+ */
+void times_table(void)
+{
+	/*Declaring Variables*/
+	int i, j, cmp;
+	/*initialization*/
+	i = j = 0;
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			cmp = i * j;
+			_putchar(cmp + '0');
+			_putchar(',');
+			if (j <= 8)
+			{
+				_putchar('.');
+				_putchar('.');
+			}else
+			{
+				_putchar('$');
+				_putchar('\n');
+			}
+			j++;
+		}
+		i++
+	}
+}
