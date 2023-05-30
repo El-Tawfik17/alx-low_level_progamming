dc4dbfaa41c600c2897ab6eeaa0fb2d2bbd2371a
El-Tawfik17 <kifwat17@gmail.com>
2023-05-30 07:51:12 -0700

    task  10 Done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - function 
+ * Description:This function adds two 
+ * integer and return the result
+ * @n:parameter of function
+ * @m:parameter of function
+ */
+int add(int n, int m)
+{
+	/*Declaring Variable*/
+	int rslt;
+	/*initialization*/
+	rslt = n + m;
+	return rslt;
+}
+
