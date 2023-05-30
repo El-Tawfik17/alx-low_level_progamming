936fa7b5bc423b9a4580b5f63fa9ccbd8e515deb
El-Tawfik17 <kifwat17@gmail.com>
2023-05-29 21:46:49 -0700

    task 1 Done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description:This program prints alphabet
+ * in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/*declaring variable*/
+	char c;
+	/*initialization*/
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar('c');
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
