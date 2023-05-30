362517a5334ef0dee589bf90bb1e441e30983830
El-Tawfik17 <kifwat17@gmail.com>
2023-05-29 22:49:07 -0700

    task 4 Done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,7 @@
  * _isalpha - function
  * Description:This function checks alphabet
  * it return 1 if it is alphabet and 0 otherwhise
+ * @c:is parameter for our function
  * Return:1 or 0
  */
 int _isalpha(int c)
