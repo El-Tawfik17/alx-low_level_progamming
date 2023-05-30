20bdb39c8d4e7274d4a9690bc3cdc8839942da82
El-Tawfik17 <kifwat17@gmail.com>
2023-05-29 23:08:07 -0700

    task 5 Done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - function
+ * Description:This function checks sign
+ * it return 1 if it is postive and 0 or -1 otherwhise
+ * @n:is parameter for our function
+ * Return:1 -1  or 0
+ */
+int _print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
