224811ac76579cf69ba68935db9c16c7a6f20a74
El-Tawfik17 <kifwat17@gmail.com>
2023-05-31 12:44:39 -0700

    task 15 Done

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:This program  prints
+ * the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	/*Declaring variables*/
+	long i = 0, ii = 1, iii = 0;
+	int j = 0;
+
+	while (j < 98)
+	{
+		iii = ii + i;
+		if (j <= 96)
+			printf("%ld, ", iii);
+		else
+			printf("%ld", iii);
+		i = ii;
+		ii = iii;
+		j++;
+	}
+	printf("\n");
+	return (0);
+}
+
+
