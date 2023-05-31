766d0a9bdf36ec0cc0a7e237d0978a805dac0ac1
El-Tawfik17 <kifwat17@gmail.com>
2023-05-31 07:08:25 -0700

    task 12 Done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,14 +3,13 @@
  * print_times_table - function
  * Description:This function prints the 9 times
  * tables,starting with 0
- * @n:parameter of function
+ * @n :parameter of function
  */
 void print_times_table(int n)
 {
 	/*Declaring Variables*/
-	int i, j, cmp, lstd, fstd, scdd;
-	/*initialization*/
-	i = j = 0;
+	int i = 0, j = 0, cmp;
+
 	if (n <= 15 && n > 0)
 	{
 		while (i < n)
@@ -22,16 +21,25 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
-				} else if ((j > 0) && (cmp > 9))
-				{
-					_putchar(' ');
-					_putchar(' ');
-				} else if ((j > 0) && (cmp > 9) && (cmp > 99))
+				} else if (j > 0 && cmp > 9)
 				{
 					_putchar(' ');
 				}
-				 nat_print(i, j, cmp, n);
+				if (cmp <= 9)
+				{
+					_putchar(cmp + '0');
+				} else if (cmp > 9 && cmp <= 99)
+				{
+					_putchar(((cmp - (cmp % 10)) / 10) + '0');
+					_putchar((cmp % 10) + '0');
+				} else if (cmp > 99)
+				{
+					_putchar(((cmp - (cmp % 100)) / 100) + '0');
+					_putchar((((cmp % 100) - (cmp % 10)) / 10) + '0');
+					_putchar((cmp % 10) + '0');
+				}
+				if (j <= (n - 2))
+					_putchar(',');
 				j++;
 			}
 			_putchar('\n');
