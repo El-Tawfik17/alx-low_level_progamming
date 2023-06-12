9fdef09ffc724faf4db8aaf9658362d0fa818762
El-Tawfik17 <kifwat17@gmail.com>
2023-06-11 21:41:13 -0700

    task 3 Done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,11 @@ int _strcmp(char *s1, char *s2)
 	int i, j;
 	/*searching s1 and s2 length*/
 	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
 	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
 	if ((i) > (j))
 		return (15);
 	if ((i) < (j))
