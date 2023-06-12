3f8ba0c080473e4633b54416ee82d7c3cba820e7
El-Tawfik17 <kifwat17@gmail.com>
2023-06-11 21:27:47 -0700

    task 3 Done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - function
+ * Description:This function compares two strings
+ * @s1:parameter of function
+ * @s2:parameter of function
+ * Return: 1 , 0 or less than 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	/*declaring variable*/
+	int i, j;
+	/*searching s1 and s2 length*/
+	for (i = 0; s1[i] != '\0'; i++)
+	for (j = 0; s1[j] != '\0'; j++)
+	if ((i - 1) > (j - 1))
+		return (1);
+	if ((i - 1) < (j - 1))
+		return (-1);
+	return (0);
+}
+
