7628305e6f4bcf968ea6f0861a84ea7357a6f8c4
El-Tawfik17 <kifwat17@gmail.com>
2023-06-13 08:36:16 -0700

    task 2 Done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - function
+ * Description:This function locates a character in a string.
+ * @s:parameter of function
+ * @c:parameter of function
+ * Return:pointer or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	/*Declaring variable*/
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (NULL);
+}
+
