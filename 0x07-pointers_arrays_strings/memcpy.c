04fcd857e9f26626cfdde2888d090f7a7a6db931
El-Tawfik17 <kifwat17@gmail.com>
2023-06-13 07:59:56 -0700

    task 1 Done

---
+++ 0x07-pointers_arrays_strings/memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - function
+ * Description:This function copies memoy area
+ * @dest:parameter of function
+ * @src:parameter of function
+ * @n:parameter of function
+ * Return:pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	/*declaring variable*/
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (0);
+}
