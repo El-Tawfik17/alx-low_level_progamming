ef878d3b506b29fe3662431efb002c64cecb77e5
El-Tawfik17 <kifwat17@gmail.com>
2023-06-17 07:43:29 -0700

    task 3 Done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description:this program prints
+ * multiple of arguments passed into it
+ * @argc:parameter of prrogram
+ * @argv:parameter of prrogram
+ * Return: 0(sucess)
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+			mul *= atoi(argv[i]);
+		printf("%d\n", mul);
+	}
+	return (0);
+}
