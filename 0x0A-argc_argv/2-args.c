a9112f738cd20d7e58f8ac3dbe7bd9dc8a9dcd50
El-Tawfik17 <kifwat17@gmail.com>
2023-06-17 07:17:40 -0700

    task 2 Done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description:this program prints
+ * arguments passed into it
+ * @argc:parameter of prrogram
+ * @argv:parameter of prrogram
+ * Return: 0(sucess)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+	return (0);
+}
