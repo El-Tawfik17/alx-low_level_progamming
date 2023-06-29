699930c7dea3deba1a89cafd1c95e86e6f70a90d
El-Tawfik17 <kifwat17@gmail.com>
2023-06-28 19:25:54 -0700

    task 4 Done

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _copyto - function
+ * Description:This function copy from the old
+ * array to the new
+ * @p:paameter of function
+ * @pp:paameter of function
+ * @size:paameter of function
+ * Return:nothing
+ */
+void _copyto(void *p, void *pp, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		p[i] = pp[i];
+}
+/**
+ * _realloc - function
+ * Description:This function reallocates a memory block
+ * using malloc and free
+ * @ptr:parameter of function
+ * @old_size:parameter of function
+ * @new_size:parameter of function
+ * Return:pointer(success) or NULL(failure)
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *nptr;
+
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (0);
+	}
+	nptr = malloc(new_size);
+	if (nptr == NULL)
+		return (0);
+	if (old_size < new_size)
+		_copyto(ptr, nptr, old_size);
+	else
+		_copyto(ptr, nptr, new_size);
+	free(ptr);
+	return (nptr);
+}
